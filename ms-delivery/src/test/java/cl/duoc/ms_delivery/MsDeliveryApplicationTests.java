6d38586977ee97d2fceac328a332297afa204a84
Amira <amiraa@live.cl>
2026-05-19 02:20:05 -0400

    Added ms-delivery baseline

---
+++ ms-delivery/src/test/java/cl/duoc/ms_delivery/MsDeliveryApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.duoc.ms_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
