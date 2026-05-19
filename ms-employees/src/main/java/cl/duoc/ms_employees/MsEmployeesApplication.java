a54c4c29222cd35ea3b7202840a1140942ba9fb3
4cr1l1c0 <4cr1l1c0@gmail.com>
2026-05-19 01:41:21 -0400

    adding ms-employees base

---
+++ ms-employees/src/main/java/cl/duoc/ms_employees/MsEmployeesApplication.java
@@ -0,0 +1,14 @@
+package cl.duoc.ms_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MsEmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEmployeesApplication.class, args);
+    }
+}
---
+++ ms-employees/src/test/java/cl/duoc/ms_employees/MsEmployeesApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.duoc.ms_employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
