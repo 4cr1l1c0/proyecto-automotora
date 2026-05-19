57a7b347f2c72b7d12501d625e295c6fb7284967
4cr1l1c0 <4cr1l1c0@gmail.com>
2026-05-19 02:04:57 -0400

    adding ms-inventory base

---
+++ ms-inventory/src/main/java/cl/duoc/ms_inventory/MsInventoryApplication.java
@@ -0,0 +1,14 @@
+package cl.duoc.ms_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MsInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsInventoryApplication.class, args);
+    }
+}
---
+++ ms-inventory/src/test/java/cl/duoc/ms_inventory/MsInventoryApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.duoc.ms_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
