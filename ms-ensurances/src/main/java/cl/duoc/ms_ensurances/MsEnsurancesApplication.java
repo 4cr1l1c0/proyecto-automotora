6c2d57aac3b610bf2caf88ab904260c8a124f081
4cr1l1c0 <4cr1l1c0@gmail.com>
2026-05-19 20:01:26 -0400

    adding ms-ensurances base

---
+++ ms-ensurances/src/main/java/cl/duoc/ms_ensurances/MsEnsurancesApplication.java
@@ -0,0 +1,14 @@
+package cl.duoc.ms_ensurances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MsEnsurancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEnsurancesApplication.class, args);
+    }
+}
---
+++ ms-ensurances/src/test/java/cl/duoc/ms_ensurances/MsEnsurancesApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.duoc.ms_ensurances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEnsurancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
